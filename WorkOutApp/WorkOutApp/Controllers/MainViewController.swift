06cd69a2e20663d2c862b0d3715919b0472b163c
TonyJem <a.jemanov@gmail.com>
2022-01-16 16:46:37 +0200

    Add userPhotoImageView to MainViewController

--- WorkOutApp/WorkOutApp/Controllers/MainViewController.swift
+++ WorkOutApp/WorkOutApp/Controllers/MainViewController.swift
@@ -1,6 +1,15 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private let userPhotoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = #colorLiteral(red: 0.7607843137, green: 0.7607843137, blue: 0.7607843137, alpha: 1)
+        imageView.layer.borderWidth = 5
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,12 +20,19 @@ class MainViewController: UIViewController {
     
     private func setupViews() {
         view.backgroundColor = .specialBackground
+        
+        view.addSubview(userPhotoImageView)
     }
     
 }
 
 extension MainViewController {
     private func setConstraints() {
-        
+        NSLayoutConstraint.activate([
+            userPhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            userPhotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            userPhotoImageView.heightAnchor.constraint(equalToConstant: 100),
+            userPhotoImageView.widthAnchor.constraint(equalToConstant: 100)
+        ])
     }
 }
