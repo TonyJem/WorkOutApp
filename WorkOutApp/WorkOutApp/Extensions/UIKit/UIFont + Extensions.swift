da5f2cf3f7279b1cb8592927732ab164f2cf5377
TonyJem <a.jemanov@gmail.com>
2022-01-16 15:53:59 +0200

    Add extensions

---
+++ WorkOutApp/WorkOutApp/Extensions/UIKit/UIFont + Extensions.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//label.font = UIFont(name: "Roboto-Medium", size: 24)
+
+extension UIFont {
+    //Medium
+    static func robotoMedium12() -> UIFont? {
+        return UIFont.init(name: "Roboto-Medium", size: 12)
+    }
+    
+    static func robotoMedium14() -> UIFont? {
+        return UIFont.init(name: "Roboto-Medium", size: 14)
+    }
+    
+    static func robotoMedium18() -> UIFont? {
+        return UIFont.init(name: "Roboto-Medium", size: 18)
+    }
+    
+    static func robotoMedium24() -> UIFont? {
+        return UIFont.init(name: "Roboto-Medium", size: 24)
+    }
+    
+    //Bold
+    static func robotoBold16() -> UIFont? {
+        return UIFont.init(name: "Roboto-Bold", size: 16)
+    }
+    
+    static func robotoBold20() -> UIFont? {
+        return UIFont.init(name: "Roboto-Bold", size: 20)
+    }
+}
