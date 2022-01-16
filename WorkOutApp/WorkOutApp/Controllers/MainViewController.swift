f84bb62ff69ca2f3aaabcb63a6aefafc892bdf48
TonyJem <a.jemanov@gmail.com>
2022-01-16 16:30:20 +0200

    Set real background color for MainViewController

---
+++ WorkOutApp/WorkOutApp/Controllers/MainViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .specialBackground
+    }
+    
+}
+
+extension MainViewController {
+    private func setConstraints() {
+        
+    }
+}
