e63961226c1d1ce6ba8ae7766c8e3510401feab6
TonyJem <a.jemanov@gmail.com>
2022-01-19 09:12:01 +0200

    Add nameTextField to NewWorkoutViewController

--- WorkOutApp/WorkOutApp/Controllers/NewWorkoutViewController.swift
+++ WorkOutApp/WorkOutApp/Controllers/NewWorkoutViewController.swift
@@ -23,6 +23,21 @@ class NewWorkoutViewController: UIViewController {
     
     private let nameLabel = UILabel(text: "Name")
     
+    private let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .specialBrown
+        textField.borderStyle = .none
+        textField.layer.cornerRadius = 10
+        textField.textColor = .specialGray
+        textField.font = .robotoBold20()
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .done
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
     // MARK: - LifeCycle
     override func viewDidLayoutSubviews() {
         closeButton.layer.cornerRadius = closeButton.frame.height / 2
@@ -33,6 +48,7 @@ class NewWorkoutViewController: UIViewController {
         
         setupViews()
         setConstraints()
+        setDelegates()
     }
     
     // MARK: - Actions
@@ -48,10 +64,23 @@ class NewWorkoutViewController: UIViewController {
         view.addSubview(newWorkoutLabel)
         view.addSubview(closeButton)
         view.addSubview(nameLabel)
+        view.addSubview(nameTextField)
+    }
+    
+    private func setDelegates() {
+        nameTextField.delegate = self
     }
 }
 
-//MARK: - SetConstraints
+// MARK: - UITextFieldDelegate
+extension NewWorkoutViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameTextField.resignFirstResponder()
+    }
+}
+
+// MARK: - SetConstraints
 extension NewWorkoutViewController {
     
     private func setConstraints() {
@@ -71,6 +100,13 @@ extension NewWorkoutViewController {
             nameLabel.topAnchor.constraint(equalTo: newWorkoutLabel.bottomAnchor, constant: 10),
             nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
             nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 3),
+            nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            nameTextField.heightAnchor.constraint(equalToConstant: 38)
         ])
     }
 }
