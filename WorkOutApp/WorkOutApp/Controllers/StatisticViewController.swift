d77250c4ecde672449a44c21eba27e9be78d0af0
TonyJem <a.jemanov@gmail.com>
2022-01-19 10:57:35 +0200

    Create and implement MainTabBarController as an initial VC

---
+++ WorkOutApp/WorkOutApp/Controllers/StatisticViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class StatisticViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
