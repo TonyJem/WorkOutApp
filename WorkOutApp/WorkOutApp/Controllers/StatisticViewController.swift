fe1639a4928f552eac89097c1d3c5f37156e5bbf
TonyJem <a.jemanov@gmail.com>
2022-02-03 22:49:25 +0200

    Add statisticLabel to StatisticViewController

--- WorkOutApp/WorkOutApp/Controllers/StatisticViewController.swift
+++ WorkOutApp/WorkOutApp/Controllers/StatisticViewController.swift
@@ -2,9 +2,36 @@ import UIKit
 
 class StatisticViewController: UIViewController {
     
+    private let statisticLabel: UILabel = {
+       let label = UILabel()
+        label.text = "STATISTIC"
+        label.font = .robotoMedium24()
+        label.textColor = .specialGray
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .specialBackground
+        
+        view.addSubview(statisticLabel)
+    }
+}
 
-        view.backgroundColor = .systemPink
+extension StatisticViewController {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            statisticLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            statisticLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 }
