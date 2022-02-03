16ff16cae9d43435a146948f2959071c9af9f8fa
TonyJem <a.jemanov@gmail.com>
2022-02-03 22:53:01 +0200

    Add segmentedControl to StatisticViewController

--- WorkOutApp/WorkOutApp/Controllers/StatisticViewController.swift
+++ WorkOutApp/WorkOutApp/Controllers/StatisticViewController.swift
@@ -3,13 +3,30 @@ import UIKit
 class StatisticViewController: UIViewController {
     
     private let statisticLabel: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.text = "STATISTIC"
         label.font = .robotoMedium24()
         label.textColor = .specialGray
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private let segmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["Week", "Month"])
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.backgroundColor = .specialGreen
+        segmentedControl.selectedSegmentTintColor = .specialYellow
+        let font = UIFont(name: "Roboto-Medium", size: 16)
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: font as Any,
+                                                 NSAttributedString.Key.foregroundColor: UIColor.white],
+                                                for: .normal)
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: font as Any,
+                                                 NSAttributedString.Key.foregroundColor: UIColor.specialGray],
+                                                for: .selected)
+        segmentedControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        return segmentedControl
     }()
     
     override func viewDidLoad() {
@@ -19,10 +36,15 @@ class StatisticViewController: UIViewController {
         setConstraints()
     }
     
+    @objc private func segmentChanged() {
+        print("🟢 segmentChanged")
+    }
+    
     private func setupViews() {
         view.backgroundColor = .specialBackground
         
         view.addSubview(statisticLabel)
+        view.addSubview(segmentedControl)
     }
 }
 
@@ -32,6 +54,12 @@ extension StatisticViewController {
         NSLayoutConstraint.activate([
             statisticLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             statisticLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            segmentedControl.topAnchor.constraint(equalTo: statisticLabel.bottomAnchor, constant: 10),
+            segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
     }
 }
